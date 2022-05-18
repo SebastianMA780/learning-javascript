90d1872dea6e8bcc1ee75efcd7a9587de32a00ff
Sebastian Marín Arias <sebastianmarin780@gmail.com>
2022-05-18 11:59:00 -0500

    http methods, http status codes info added

--- src/api-rest/index.js
+++ src/api-rest/index.js
@@ -1,17 +1,54 @@
-import fetch from 'node-fetch';//library to simulate window.fetch in node;
+/* URL STRUCTURE TO MAKE API REST REQUEST */
 
 const START_DATE = '2020-01-01';
 const END_DATE = '2020-01-04';
-const API_KEY = 'DEMO_KEY';
+const API_KEY = 'DEMO_KEY'; 
+/*
+ Api keys is one of the ways to identify who is making requests.
+ Authentication !== Authorization 
+*/
+
 const NASA_URL = 'https://api.nasa.gov/neo/rest/v1/feed'+//this is called an endpoint,
-//it is meant to give specific info from the data base.
+//it is meant to give specific info from the server.
 `?start_date=${START_DATE}&end_date=${END_DATE}&api_key=${API_KEY}`;
 /* this part of url are query parameters, key values pairs 
 that contains info to filter actions of the server.
 Structure: they are preceded of '?' and separate with '&'.*/
+
+/* REQUESTING */
+import fetch from 'node-fetch';//library to simulate window.fetch in node;
 
 fetch(NASA_URL) 
 //This will return a promise with data from the server.
 //Server return an object as promise with lot of data like headers, http status etc.
 	.then(res => res.json())  //.json() will return the body as a promise with json content.
 	.then( data => console.log({ data }));
+
+/* 
+	HTTP Status code: is a message that website's server 
+	sends to the browser to indicate request state info.
+*/
+// https://www.webfx.com/web-development/glossary/http-status-codes/
+// 1XX Informational
+// 2XX Success
+// 3XX Redirection
+// 4XX Client Error
+// 5XX Server Error
+fetch(NASA_URL) 
+	.then(res => console.log({ status: res.status }));
+
+/* 
+HTTP METHODS: set of request methods to indicate the desired action to be performed in the server.
+	//E.X
+	//get
+	//post
+	//put
+	//patch
+	//delete
+*/
+fetch(NASA_URL , {
+	method: "GET"
+}) 
+	.then(res => res.json())
+	.then( data => console.log({ dataHttpMethod: data }));
+
