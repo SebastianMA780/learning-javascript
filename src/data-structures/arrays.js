1f061fa01d6e6e6c5ca42fc9552b0d42697ff1a9
Sebastian Marín Arias <sebastianmarin780@gmail.com>
2022-05-25 20:32:25 -0500

    arrays data structure added

---
+++ src/data-structures/arrays.js
@@ -0,0 +1,7 @@
+/* Arrays or Lists are ordered collection of information addressed by indexes */
+
+//There are arrays of type dynamics and statics.
+//-dynamics: Amount of memory undefined, this allows you to make modifications to them.
+//-statics: A specific amount of memory assigned 
+
+/* Js set arrays type dynamic by the default */
