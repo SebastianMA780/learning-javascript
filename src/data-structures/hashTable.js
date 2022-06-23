553475f6d733bfeaf30a4fdf4bde35a6c945e82d
Sebastian Marín Arias <sebastianmarin780@gmail.com>
2022-06-22 20:34:02 -0500

    hash table example

--- src/data-structures/hashTable.js
+++ src/data-structures/hashTable.js
@@ -18,4 +18,59 @@
 	- hash table collision: two keys or more could be allocated with the same hash.
 
 */
+	class HashTable {
 
+		constructor(size) {
+			this.data = new Array(size);
+		}
+
+		hashFunction(key) {
+			let hash = 0
+			for (let index = 0; index < key.length; index++) {
+				hash = (hash + key.charCodeAt(index) * index) % this.data.length;
+			}
+			return hash;
+		}
+
+		set(key, value) {
+			const address = this.hashFunction(key);
+			console.log(`address: ${address}`);
+			if(!this.data[address]) {
+				this.data[address] = [];
+			}
+			this.data[address].push([key, value]);
+			return this.data;
+		}
+
+		get(key) {
+			const address = this.hashFunction(key);
+			const currentBucket = this.data[address];
+			if(currentBucket) {
+				for (let index = 0; index < currentBucket.length; index++) {
+					if(currentBucket[index][0] === key) {
+						return currentBucket[index][1];
+					}
+				}
+			}
+			return undefined;
+		}
+
+
+	}
+
+	const myHashTable = new HashTable(50);
+	console.log({
+		myHashTable
+	});
+	myHashTable.set('engineer', 29);
+	myHashTable.set('lawyer', 30);
+	myHashTable.set('doctor', 31);
+	myHashTable.set('engineer', 32);
+	console.log({
+		myHashTable,
+		getQuantity: [
+			myHashTable.get('lawyer'),
+			myHashTable.get('engineer'),
+			myHashTable.get('doctor'),
+		]
+	});
