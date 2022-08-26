b03eb78d35f5ed1cd2f3739e842907502d6faf3c
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-08-26 11:32:55 -0500

    async / await warning

--- src/asynchronism/async-await/index.js
+++ src/asynchronism/async-await/index.js
@@ -1,1 +1,34 @@
-console.log('async-await');
+
+
+
+
+
+
+
+//Be careful when use async / await because of it could lead to bad performance as follows:
+
+	const asyncTime = async () => {
+		const response1 = await new Promise(resolve => setTimeout(() => resolve(1), 3000));
+		const response2 = await new Promise(resolve => setTimeout(() => resolve(2), 3000));
+		const response3 = await new Promise(resolve => setTimeout(() => resolve(3), 3000));
+		console.log([response1, response2, response3]);
+		/* In each await the function execution is stopped until promise will be resolved.
+		So the final time in the execution function will be the sum of the 3 promises. */
+	}
+
+	asyncTime(); // Execution Time: 9 seconds;
+
+	const promisesTime = async () => {
+		const response = await Promise.all([
+			new Promise(resolve => setTimeout(() => resolve(1), 3000)),
+			new Promise(resolve => setTimeout(() => resolve(2), 4000)),
+			new Promise(resolve => setTimeout(() => resolve(3), 3000)),
+		]);
+		console.log(response);
+		/* unlike of async / await the three requests will be launched in a chain and 
+		the resolution time will be not a sum but when all are complete.
+		So in this example they will all be resolved in 4 seconds and
+		not in 10 if we wait for each request to continue with the other. */
+	}
+
+	promisesTime(); // Execution Time: 4 seconds;
