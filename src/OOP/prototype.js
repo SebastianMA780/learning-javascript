eb5aacdedd5634581274962dbb9d21338439bb6e
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-08-29 19:33:40 -0500

    objected oriented programing added

---
+++ src/OOP/prototype.js
@@ -0,0 +1,36 @@
+/* 
+
+	Object  Literal: are comma-separated list of name-value pairs inside curly braces
+	e.x: 
+	const greeting = {
+    fullName: "Michael Jackson",
+    greet: (message, name) => {
+        console.log(message + " " + name + "!!");
+    }
+	};
+
+	* Object  Literal !== instances.
+	* These object are not the same than objects in object oriented programming
+
+*/
+
+/* 
+	Prototype: 
+		- JavaScript has some prototypes by default.
+			* Object
+			* Array
+
+	function Student() {
+		this.name = 'Name'
+		this.age = 18
+	}
+
+	const cooper = {
+		name: 'Shelly'
+		age: 20
+	}
+
+	const smith = new Student(); // prototype instance
+	// smith object is different from a object literal
+
+*/
--- src/data-structures/1-theory.md
+++ src/data-structures/1-theory.md
@@ -1,3 +1,4 @@
 # Data Structures.
 
 are a **collection of values**, relationship between them and methods to access the information or manipulate it.
+[Data structure Practice](https://leetcode.com/explore/learn/).
