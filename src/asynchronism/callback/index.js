d2fd703ba2a73324914584e993c71c7863e6158b
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-09-01 15:44:35 -0500

    callback explanation added

--- src/asynchronism/callback/index.js
+++ src/asynchronism/callback/index.js
@@ -1,1 +1,50 @@
-console.log('callback');
+/* 
+	A callback is a function passed as an argument to another function,
+	this technique allows a function to call another function.
+	This is useful to control the function execution sequence.
+*/
+
+function sum(num1, num2) {
+	return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+	return callback(num1, num2);
+}
+
+console.log(calc(2, 2, sum));
+
+//Api-rest
+// {
+//   "characters": "https://rickandmortyapi.com/api/character",
+//   "locations": "https://rickandmortyapi.com/api/location",
+//   "episodes": "https://rickandmortyapi.com/api/episode"
+// }
+
+import XMLHttpRequest from 'xmlhttprequest';
+const API = ' https://rickandmortyapi.com/api';
+
+
+function fetchData(urlApi, callback) {
+	let xhttp = new XMLHttpRequest.XMLHttpRequest();
+	xhttp.open('GET', urlApi, true);
+	xhttp.onreadystatechange = (event) => {
+		if (xhttp.readyState === 4) {
+			if (xhttp.status === 200) {
+				callback(null, JSON.parse(xhttp.responseText))
+			}
+		}
+	}
+	xhttp.send();
+}
+
+fetchData(`${API}/character`, (error, data) => {
+	if (error) return console.error(error);
+	console.log(data);
+	fetchData(`${API}/character/${data.results[0].id}`, (error2, data2) => { 
+		if (error2) return console.error(error2);															
+		console.log(data2);
+	});
+	//Callback Hell!!!!
+	// this code becomes in bunch of nested requests
+});
--- src/data-structures/1-theory.md
+++ src/data-structures/1-theory.md
@@ -1,4 +1,5 @@
 # Data Structures.
 
 are a **collection of values**, relationship between them and methods to access the information or manipulate it.
-[Data structure Practice](https://leetcode.com/explore/learn/).
+- [Data Structure Theory](https://github.com/trekhleb/javascript-algorithms).
+- [Data structure Practice](https://leetcode.com/explore/learn/).
