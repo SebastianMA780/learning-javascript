616212d6401493dbb3d334d29933eb6f7f29251e
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-12-27 10:08:47 -0500

    IIFE pros and cons added

--- src/concepts/IIFE.js
+++ src/concepts/IIFE.js
@@ -3,7 +3,28 @@
 
 	- Is a function that is executed immediately after it is defined.
 
-		* Main benefit is data privacy and quick execution.
+	Pros:
+
+	* IIFEs can create a private scope for variables and functions, 
+		helping to prevent pollution of the global scope and to protect against variable collision.
+
+	* IIFEs can be used to create modular, reusable code, 
+		especially when combined with module patterns.
+
+	* IIFEs can help to improve the structure and organization of code 
+		by grouping related code together in a self-contained function.
+
+Cons:
+
+	* IIFEs can make code harder to understand, 
+		especially for developers who are not familiar with this pattern.
+
+	* IIFEs can increase the size of the code by requiring the function definition 
+		and invocation to be written in a single line of code.
+
+	* IIFEs can be less efficient than other patterns, such as 
+		object-oriented programming or the module pattern, because 
+		they require the function to be defined and invoked every time it is used.
 */
 
 (function() {
