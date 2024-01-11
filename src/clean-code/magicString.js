ef70c65560fc17db05629afc9ab02831cb6f1b50
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2024-01-11 10:01:38 -0500

    adds magic string topic improvement code

---
+++ src/clean-code/magicString.js
@@ -0,0 +1,22 @@
+export const MESSAGE_TYPE = Object.freeze({ // freeze to make sure no one can change the value
+  ERROR: 'error',
+  WARNING: 'warning',
+  SUCCESS: 'success'
+})
+
+function printMessage (message) {
+  // Magic string is 'error' used hard-coded, this is not reusable and not easy to change.
+  // instead you should use different strategy like enum or object.
+  if (message === 'error') {
+    console.log('Error')
+  } else if (message === MESSAGE_TYPE.WARNING) {
+    console.log('Warning')
+  } else if (message === MESSAGE_TYPE.SUCCESS) {
+    console.log('Success')
+  } else {
+    console.log('Unknown message')
+  }
+}
+
+printMessage('error') // this could lead to error if you misspell the string or it changes in the future.
+printMessage(MESSAGE_TYPE.ERROR) // this is more readable and less error prone.
